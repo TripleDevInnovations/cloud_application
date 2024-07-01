173842b8a4c83aa5b84fcabcf6c9a1eaa701c8b8
JAHEESE <janek.heese@ottogroup.com>
2024-07-01 16:50:25 +0200

    package-info.java

--- src/test/java/com/personal_projects/cloud_application/CloudApplicationTests.java
+++ src/test/java/com/personal_projects/cloud_application/CloudApplicationTests.java
@@ -1,11 +1,10 @@
 package com.personal_projects.cloud_application;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CloudApplicationTests {
 
-  @Test
-  void contextLoads() {}
+//  @Test
+//  void contextLoads() {}
 }
